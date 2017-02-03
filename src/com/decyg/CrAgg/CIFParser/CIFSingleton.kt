b85caa603433957d565bee6fb7000929df53801f
declan <declan.g.neilson@gmail.com>
2017-02-03 16:02:34 +0000

    Fleshed out initial regex for the CIF parser, last part to do is to write the actual recursive descent parsing logic for it, should allow me to iron out any bugs whilst doing that aswell.

--- src/com/decyg/CrAgg/CIFParser/CIFSingleton.kt
+++ src/com/decyg/CrAgg/CIFParser/CIFSingleton.kt
@@ -18,8 +18,74 @@ object CIFSingleton {
      *
      */
 
+
     /**
-     * Below are the data classes representing the
+     * Below are the specific rules required for the detailed expressions above
+     */
+
+    // Character sets & basic characters
+
+    private val RX_SP =     Regex("\\x20")                      // Space
+    private val RX_HT =     Regex("\\x09")                      // Horizontal tab
+    private val RX_EOL =    Regex("(?:\\x0A|\\x0D\\x0A|\\x0D)")     // Platform agnostic line separator
+    private val RX_NEOL =   Regex("(?:[^\\x0A]|[^\\x0D\\x0A][^\\x0A\\x0D]|[^\\x0D])")                 // Anything that's not an EOL
+    private val RX_SQ =     Regex("'")                          // Single quote
+    private val RX_DQ =     Regex("\"")                         // Double quote
+
+    private val RX_OrdinaryChar =   Regex("[A-Za-z!%&()*+,\\-./0-9:<=>?@^`{|}~]")
+    private val RX_NonBlankChar =   Regex("(?:$RX_OrdinaryChar|$RX_DQ|#|$|$RX_SQ|_|;|\\[|\\])")
+    private val RX_TextLeadChar =   Regex("(?:$RX_OrdinaryChar|$RX_DQ|#|$|$RX_SQ|_|$RX_SP|$RX_HT|\\[|\\])")
+    private val RX_AnyPrintChar =   Regex("(?:$RX_OrdinaryChar|$RX_DQ|#|$|$RX_SQ|_|$RX_SP|$RX_HT|;|\\[|\\])")
+
+    private val RX_DATA_ =          Regex("(?:[dD][aA][tT][aA]_)")
+    private val RX_LOOP_ =          Regex("(?:[lL][oO][oO][pP]_)")
+    private val RX_GLOBAL_ =        Regex("(?:[gG][lL][oO][bB][aA][lL]_)")
+    private val RX_SAVE_ =          Regex("(?:[sS][aA][vV][eE]_)")
+    private val RX_STOP_ =          Regex("(?:[sS][tT][oO][pP]_)")
+
+    // Complex strings, text fields and numbers
+
+    open val RX_Comments =               Regex("(?:(?:#(?:$RX_AnyPrintChar)*$RX_EOL)+)")
+    open val RX_TokenizedComments =      Regex("(?:(?:$RX_SP|$RX_HT)+$RX_Comments)")
+    open val RX_WhiteSpace =             Regex("(?:(?:$RX_SP|$RX_HT|$RX_EOL|(?:$RX_TokenizedComments))+)")
+
+    open val RX_SemiColonTextField =     Regex("(?:$RX_EOL;(?:(?:$RX_AnyPrintChar)*$RX_EOL(?:(?:$RX_TextLeadChar(?:$RX_AnyPrintChar)*)?$RX_EOL)*);)")
+    open val RX_TextField =              RX_SemiColonTextField
+    open val RX_DoubleQuotedString =     Regex("(?:$RX_DQ(?:$RX_AnyPrintChar)*$RX_DQ$RX_WhiteSpace)")
+    open val RX_SingleQuotedString =     Regex("(?:$RX_SQ(?:$RX_AnyPrintChar)*$RX_SQ$RX_WhiteSpace)")
+    open val RX_UnquotedString =         Regex("(?:$RX_EOL$RX_OrdinaryChar(?:$RX_NonBlankChar)*|(?:$RX_NEOL)(?:(?:$RX_OrdinaryChar)|;)(?:$RX_NonBlankChar)*)")
+    open val RX_CharString =             Regex("(?:(?:$RX_UnquotedString)|(?:$RX_SingleQuotedString)|(?:$RX_DoubleQuotedString))")
+
+    open val RX_Digit =              Regex("(?:[0-9])")
+    open val RX_UnsignedInteger =    Regex("(?:(?:$RX_Digit)+)")
+    open val RX_Exponent =           Regex("(?:(?:[eE]|[eE][+-])$RX_UnsignedInteger)")
+    open val RX_Integer =            Regex("(?:[+-]?$RX_UnsignedInteger)")
+    open val RX_Float =              Regex("(?:(?:$RX_Integer$RX_Exponent)|(?:[+-]?$RX_Digit*\\.$RX_UnsignedInteger)|(?:$RX_Digit+\\.(?:$RX_Exponent)?))")
+    open val RX_Number =             Regex("(?:$RX_Integer|$RX_Float)")
+    open val RX_Numeric =            Regex("(?:$RX_Number)|(?:$RX_Number\\($RX_UnsignedInteger\\))")
+
+    open val RX_Tag =    Regex("(?:_(?:$RX_NonBlankChar)+)")
+    open val RX_Value =  Regex("(?:\\.|\\?|(?:$RX_Numeric)|(?:$RX_CharString)|(?:$RX_TextField))")
+
+    /**
+     * Below are the core regex expressions that will do the majority of the parsing
+     * These expressions are mostly dependant on the terminating values they consume such as
+     * comments, nonblankchars, tags and values
+     */
+
+    open val RX_LoopHeader = Regex("$RX_LOOP_(?:$RX_WhiteSpace$RX_Tag)+")
+    open val RX_LoopBody = Regex("$RX_Value(?:$RX_WhiteSpace$RX_Value)*")
+    open val RX_DataItems = Regex("(?:$RX_Tag$RX_WhiteSpace$RX_Value)|(?:$RX_LoopHeader$RX_LoopBody)")
+    open val RX_SaveFrameHeading = Regex("$RX_SAVE_(?:$RX_NonBlankChar)+")
+    open val RX_SaveFrame = Regex("$RX_SaveFrameHeading(?:$RX_WhiteSpace$RX_DataItems)+$RX_WhiteSpace$RX_SAVE_")
+    open val RX_DataBlockHeading = Regex("$RX_DATA_(?:$RX_NonBlankChar)+")
+    open val RX_DataBlock = Regex("$RX_DataBlockHeading(?:$RX_WhiteSpace(?:(?:$RX_DataItems)|(?:$RX_SaveFrame)))*")
+    open val RX_CIF = Regex("(?:$RX_Comments)?(?:$RX_WhiteSpace)?(?:$RX_DataBlock(?:$RX_WhiteSpace$RX_DataBlock)*(?:$RX_WhiteSpace)?)?")
+
+
+
+    /**
+     * Below are the data classes representing the hierarchical data structure of CIF objects
      */
     data class CIF(val comments : String = "", var dataBlocks : List<DataBlock>)
 
